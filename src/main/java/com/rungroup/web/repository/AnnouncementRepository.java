b1b8ce4730d62784910cda8efabe434b1f9cf3a0
Plamen <pslavchev1102@gmail.com>
2025-06-11 23:11:37 +0300

    SC-7: Connect Announcements with Courses

--- src/main/java/com/rungroup/web/repository/AnnouncementRepository.java
+++ src/main/java/com/rungroup/web/repository/AnnouncementRepository.java
@@ -1,7 +1,15 @@
 package com.rungroup.web.repository;
 
 import com.rungroup.web.models.Announcement;
+import com.rungroup.web.models.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AnnouncementRepository extends JpaRepository<Announcement, Long> {
+
+    List<Announcement> findByCreatedBy(UserEntity creator);
+
+
+    List<Announcement> findByCourseIdIn(List<Long> courseIds);
 }
